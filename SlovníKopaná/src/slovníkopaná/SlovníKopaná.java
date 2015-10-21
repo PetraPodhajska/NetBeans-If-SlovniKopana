d365f0141c7740eb1dd690389202a1d49b6a3e24
petra.podhajska <petra.podhajska@A211-07.pslib.cz>
2015-10-21 11:36:04 +0200

    Funční program z hodiny

--- SlovníKopaná/src/slovníkopaná/SlovníKopaná.java
+++ SlovníKopaná/src/slovníkopaná/SlovníKopaná.java
@@ -1,21 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package slovníkopaná;
 
-/**
- *
- * @author petra.podhajska
- */
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class SlovníKopaná {
 
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String[] args) {
-        // TODO code application logic here
+        ArrayList<String> seznam = new ArrayList<>();
+        Scanner vstup = new Scanner(System.in);
+        String poslední = "";
+        System.out.print("Vlož slovo: ");
+        String něco = vstup.nextLine();
+        // opakuj dokud není zadán prázdný vstup
+        while (! něco.isEmpty()) {
+            if (! poslední.isEmpty() && 
+                    něco.charAt(0)!= poslední.charAt (poslední.length()-1)) {
+                System.out.println("Slovo nezačíná na poslední písmeno.");
+                System.out.println("Vstup neplatí.");
+            } else if (seznam.contains(něco)) {
+                System.out.println("Tohle slovo už bylo. Vstup neplatí.");
+            } else {    
+                seznam.add(něco);
+            }
+            poslední = něco;
+            System.out.print("Vlož slovo: ");
+            něco = vstup.nextLine();
+        }
     }
     
 }
